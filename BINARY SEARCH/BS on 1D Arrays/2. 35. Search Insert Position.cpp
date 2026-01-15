f863698bed3ee8c45d9447443e9105666b3efe5a
Lokesh Dilip Bandurkar <lokeshdan770@gmail.com>
2026-01-15 23:45:49 +0530

    15-01-2026

---
+++ BINARY SEARCH/BS on 1D Arrays/2. 35. Search Insert Position.cpp
@@ -0,0 +1,27 @@
+/*
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        // Binary Search
+
+        int n = nums.size();
+        int start = 0, end = n - 1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(target > nums[mid]) start = mid + 1;
+            
+            else end = mid - 1;
+        }
+
+        return start;
+    }
+};
+
+*/
